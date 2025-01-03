17f9e922c077945e195feabca43a8190ace811de
tikature <detikaa10@gmail.com>
2025-01-03 16:13:06 +0700

    UP ONBOARDING

---
+++ cots/lib/modules/onboarding/controller/onboarding_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController {
+  final PageController pageController = PageController();
+  final RxInt currentPage = 0.obs;
+
+  void onPageChanged(int page) {
+    currentPage.value = page;
+  }
+}
